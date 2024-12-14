c50a73d95bd2d27acc7dfd1021ee282a66100990
Kunal Raj Pal <kunalrajpal03@gmail.com>
2024-12-14 11:23:44 +0530

    allowig host

--- server/settings.py
+++ server/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!5vntu!$(oyjy#sglwbn7ref&=in^3cxi#ze-#6xb8_c8!l!i%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['portfolio-api-wnrc.onrender.com','127.0.0.1','localhost']
 
